71064386acea36c8209fa29b116b390bd255a53b
JaeWon-Sim <kion2313@naver.com>
2025-10-16 09:20:08 +0900

    Add files via upload

---
+++ spring_mes/src/main/java/kr/or/mes2/controller/CodeMasterController.java
@@ -0,0 +1,16 @@
+package kr.or.mes2.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+import kr.or.mes2.service.CodeMasterService;
+
+@Controller
+public class CodeMasterController {
+
+	@Autowired
+	public CodeMasterService srv;
+	
+	@RequestMapping("/code")
+	public 
+}
